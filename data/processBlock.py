16327edf724303fffa4d05d8b69d0c9dddd7d977
PastaLaPate <al.bekaert@gmail.com>
2024-12-02 21:14:53 +0100

    Preprocess Blocks

--- data/processBlock.py
+++ data/processBlock.py
@@ -2,23 +2,34 @@ from IAM import split_into_blocks
 import os
 import cv2
 from torchvision.transforms import ToTensor, ToPILImage
-import PIL.Image as PIL
 from multiprocessing import Pool
 from tqdm import tqdm
 
-# All images should be the same size
+
+# Process a single file
+def process_file(args):
+    file, folderPath, newFolderPath, idx = args
+    if file.endswith('.png'):
+        loadedImg = cv2.imread(os.path.join(folderPath, file))
+        blocks = split_into_blocks(ToTensor()(loadedImg))
+        for i, block in enumerate(blocks):
+            ToPILImage()(block).save(os.path.join(newFolderPath, f"{idx * len(blocks) + i}.png"))
+
+
+# Process all files in a folder
 def process_folder(folderPath, newFolderPath):
-    files = os.listdir(folderPath)
-    # [:10] for testing
-    for _, file in enumerate(files[:10]):
-        if file.ends_with('.png'):
-            loadedImg = cv2.imread(os.path.join(folderPath, file))
-            blocks = split_into_blocks(ToTensor()(loadedImg))
-            for i, block in enumerate(blocks):
-                ToPILImage()(block).save(os.path.join(newFolderPath, f"{_*len(blocks)+i}"))
-                
+    os.makedirs(newFolderPath, exist_ok=True)
+    files = [file for file in os.listdir(folderPath) if file.endswith('.png')]
+    
+    # Prepare arguments for multiprocessing
+    args = [(file, folderPath, newFolderPath, idx) for idx, file in enumerate(files)]
+    
+    # Use a multiprocessing pool to process files
+    with Pool() as pool:
+        list(tqdm(pool.imap(process_file, args), total=len(args)))
+
 
 if __name__ == '__main__':
-    with Pool() as pool:
-        process_folder('./generated-pages', './generated-pages-blocks')
-        process_folder('./generated-nolines-pages', './generated-nolines-pages-blocks')
+    # Process folders with multiprocessing
+    process_folder('./generated-pages', './generated-pages-blocks')
+    process_folder('./generated-nolines-pages', './generated-nolines-pages-blocks')

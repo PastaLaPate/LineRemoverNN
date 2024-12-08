261f45d2029f471d0dfc2f4d629d77a6b178c091
PastaLaPate <al.bekaert@gmail.com>
2024-12-08 21:30:56 +0100

    commentary

--- train.py
+++ train.py
@@ -30,6 +30,7 @@ def collate_fn(batch):
 data_dir = './'
 
 if __name__ == '__main__':
+    # ArgParser
     argsParser = ArgumentParser(prog="LinerRemoverNN - Trainer", description="A trainer for the model")
     argsParser.add_argument("-e", "--epoch", type=int, help="The number of epoch to train the model on", required=True)
     argsParser.add_argument("-d", "--dataset", help="Dataset location", required=True)
@@ -37,11 +38,14 @@ if __name__ == '__main__':
     args = argsParser.parse_args()
 
     data_dir = args.dataset
+    # Get all needed directories paths
     pages_dir = os.path.join(data_dir, 'generated-pages')
     nolines_dir = os.path.join(data_dir, 'generated-nolines-pages')
     json_dir = os.path.join(data_dir, 'generated-words')
     pages_blocks_dir = os.path.join(data_dir, 'generated-pages-blocks')
     nolines_blocks_dir = os.path.join(data_dir, 'generated-nolines-pages-blocks')
+    
+    # Init network, dataset, dataloader, loss, and optimizer
 
     network = NeuralNetwork().to(device)
     dataset = IAMPagesSplitted(pages_blocks_dir, nolines_blocks_dir, '', readJson=False)
@@ -51,7 +55,10 @@ if __name__ == '__main__':
     loss = nn.MSELoss()
     startingEpoch = 0
     
+    # Load model if specified
+    
     if args.load:
+        # Getting the bestModel
         bestModelPath = None
         bestModelEpoch = 0
         for model in os.listdir('./models/saved/'):
@@ -67,8 +74,12 @@ if __name__ == '__main__':
             startingEpoch = bestModelEpoch+1
             network.load_state_dict(torch.load(bestModelPath, weights_only=True))
     
+    
+    # Create saved folder if not already created
     if not os.path.exists(os.path.join('./models', 'saved')):
         os.mkdir(os.path.join('./models', 'saved'))
+    
+    # Training...
     
     for epoch in range(startingEpoch, epochs):
         torch.cuda.empty_cache()
@@ -76,12 +87,13 @@ if __name__ == '__main__':
         totalLoss = 0
         
         for batch in tqdm(dataloader):
+            # Executing
             linesImages, noLines, shapes = batch
             linesImages, noLines = linesImages.to(device), noLines.to(device)
             processedFilters = network(linesImages)
             combinedFilter = torch.sum(processedFilters, dim=1, keepdim=True)  # Shape: [10, 1, 512, 512]
 
-        # Ensure the combinedFilter is normalized if needed (optional)
+             # Ensure the combinedFilter is normalized if needed (optional)
             combinedFilter = combinedFilter / processedFilters.shape[1]
 
             # Subtract the composite filter from the input
@@ -90,9 +102,11 @@ if __name__ == '__main__':
             # Compute loss between the subtracted image and the target (noLines)
             pixelLoss = loss(filteredImage, noLines)
             optimizer.zero_grad()
+            # Back prog
             batch_loss = pixelLoss.clone().detach().requires_grad_(True)
             batch_loss.backward()
             optimizer.step()
             totalLoss += pixelLoss.item()
         print(f"Epoch : {epoch} Loss : ", totalLoss/len(dataloader))
+        # Save each epoch
         torch.save(network.state_dict(), f"./models/saved/epoch-{epoch}.pt")

added transforms + fixed gradients exploding/vanishing

data/IAM.py
@@ -78,7 +78,14 @@ def reconstruct_image(blocks, original_height, original_width, block_size=512):
         blocks = [transform(np.reshape(block, (512, 512, 1))) for block in blocks]
     print(blocks[0].shape)
     channels, block_height, block_width = blocks[0].shape
-    print("Channels: ", channels, "Padded height:", padded_height, "Padded width:", padded_width)
+    print(
+        "Channels: ",
+        channels,
+        "Padded height:",
+        padded_height,
+        "Padded width:",
+        padded_width,
+    )
     reconstructed_padded_image = torch.zeros(
         (channels, padded_height, padded_width), dtype=blocks[0].dtype
     )
@@ -206,14 +213,10 @@ class IAMPagesSplitted(Dataset):
         linesImagePath = os.path.join(self.lines_pages_dir, f"{index}.png")
         noLinesImagePath = os.path.join(self.nolines_pages_dir, f"{index}.png")
         jsonPath = os.path.join(self.json_dir, f"{index}.json")
-        linesImage = (
-            read_image(linesImagePath, torchvision.io.ImageReadMode.GRAY).float()
-            / 255.0
-        )
-        noLinesImage = (
-            read_image(noLinesImagePath, torchvision.io.ImageReadMode.GRAY).float()
-            / 255.0
-        )
+        linesImage = read_image(
+            linesImagePath, torchvision.io.ImageReadMode.GRAY
+        ).float()
+        noLinesImage = read_image(noLinesImagePath, torchvision.io.ImageReadMode.GRAY)
         jsonRead = []
         if self.readJson:
             with open(jsonPath, "r") as json_file:

train.py
@@ -8,6 +8,8 @@ from models.model import NeuralNetwork
 from torch.utils.data import DataLoader
 import torch.nn as nn
 from torchvision import transforms
+import torchvision.transforms.v2 as v2
+
 # from mltu.utils.text_utils import ctc_decoder, get_cer
 # import matplotlib.pyplot as plt
 
@@ -104,15 +106,16 @@ if __name__ == "__main__":
         readJson=False,
         transform=transforms.Compose(
             [
-                transforms.RandomRotation(degrees=(0, 180)),
-                transforms.RandomPerspective(distortion_scale=0.6, p=0.75),
+                v2.RandomRotation(degrees=(0, 180)),
+                v2.RandomPerspective(distortion_scale=0.5, p=0.75),
+                v2.ToDtype(torch.float32, scale=True),
             ]
         ),
     )
     dataloader = DataLoader(
         dataset, batch_size=16, shuffle=True, collate_fn=collate_fn, num_workers=8
     )
-    optimizer = torch.optim.Adam(network.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(network.parameters(), lr=1e-5)
     epochs = args.epoch
     loss = nn.MSELoss()
     startingEpoch = 0